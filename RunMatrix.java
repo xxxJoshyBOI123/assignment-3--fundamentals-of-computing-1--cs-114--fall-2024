5284d526e41ba5a1b8d4082cbe9d8040998a6d20
JOSH <149009251+xxxJoshyBOI123@users.noreply.github.com>
2024-11-09 14:46:24 -0500

    initialize matrix with default values

--- RunMatrix.java
+++ RunMatrix.java
@@ -4,8 +4,16 @@ public class RunMatrix {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
 
+
     System.out.println("Please enter the size of your matrix: ");
     int size = input.nextInt();
-    System.out.println("Your matriz is " + size + "x" + size);
+    System.out.println("Your matrix is " + size + "x" + size);
+
+
+    int[][] matrix = new int[size][size];
+    System.out.println("Printing matrix with default values: ");
+
+
+
   }
 }
