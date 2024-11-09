97f34d020878e776bef3cb1bf905d023342c9e2e
JOSH <149009251+xxxJoshyBOI123@users.noreply.github.com>
2024-11-09 12:05:21 -0500

    martix size from user

--- RunMatrix.java
+++ RunMatrix.java
@@ -2,5 +2,10 @@ import java.util.Scanner;
 
 public class RunMatrix {
   public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Please enter the size of your matrix: ");
+    int size = input.nextInt();
+    System.out.println("Your matriz is " + size + "x" + size);
   }
 }
