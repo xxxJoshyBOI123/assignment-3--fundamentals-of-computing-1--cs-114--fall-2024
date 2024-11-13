9482568257dad27e769168f4c11bd4ed4ae686ed
JOSH <149009251+xxxJoshyBOI123@users.noreply.github.com>
2024-11-12 21:59:39 -0500

    removed size and changed to this.matrix.length

--- Matrix.java
+++ Matrix.java
@@ -1,33 +1,67 @@
 public class Matrix {
-  private int size;
   private int[][] matrix;
 
+
   public Matrix(int size) {
-    this.size = size;
     this.matrix = new int[size][size];
   }
 
   public void IntializeMatrix() {
-    for(int row = 0; row < size; row++) {
-      for(int col = 0; col < size; col++) {
+    for(int row = 0; row < this.matrix.length; row++) {
+      for(int col = 0; col < this.matrix[row].length; col++) {
         matrix[row][col] = 0;
       }
     }
   }
 
+
   public void PopulateMatrix() {
-    for(int row = 0; row < size; row++) {
-      for(int col = 0; col < size; col++) {
-        int temp = matrix[row][col];
-        matrix[row][col] = matrix[row][size - col - 1];
-        matrix[row][size - col - 1] = temp;
+    int value = 1;
+    for(int row = 0; row < this.matrix.length; row++) {
+      for(int col = 0; col < this.matrix[row].length; col++) {
+        matrix[row][col] = value++;
       }
     }
   }
-  public void printMatrix() {
-    for(int row = 0; row < size; row++) {
-      for(int col = 0; col < size; col++) {
-        System.out.print(matrix[row][col] + "\t");
+
+
+  public void FlipMatrix() {
+    for(int row = 0; row < this.matrix.length; row++) {
+      for(int col = 0; col < this.matrix[row].length; col++) {
+        if(this.IsDiagonal(row, col)) {
+          int size = this.matrix[row].length - 1;
+          swap(row, col, size - row, size - col);
+        }  else {
+             break;
+        }
+      }
+    }
+  }
+
+
+  private void swap(int row1, int col1, int row2, int col2) {
+    int temp = this.matrix[row1][col1];
+    this.matrix[row1][col1] = this.matrix[row2][col2];
+    this.matrix[row2][col2] = temp;
+  }
+
+  public boolean IsDiagonal(int row, int col) {
+    int maxIndex = this.matrix[row].length - 1;
+    return col == (maxIndex - row);
+  }
+
+
+  public void PrintMatrix() {
+    String ANSI_RESET = "\u001B[0m";
+    String ANSI_YELLOW = "\u001B[33m";
+    for(int row = 0; row < this.matrix.length; row++) {
+      for(int col = 0; col < this.matrix[row].length; col++) {
+        int value = this.matrix[row][col];
+        if(this.IsDiagonal(row, col)) {
+          System.out.printf("%s%d%s ", ANSI_YELLOW, value, ANSI_RESET);
+        } else {
+          System.out.printf("%d ", value);
+        }
       }
       System.out.println();
     }
--- RunMatrix.java
+++ RunMatrix.java
@@ -9,12 +9,27 @@ public class RunMatrix {
     int size = input.nextInt();
     System.out.println("Your matrix is " + size + "x" + size);
 
+
     Matrix table = new Matrix(size);
 
-    System.out.println("\n Populating matrix...matrix populated");
+
+    System.out.println("\n Printing matrix with default values:");
     table.IntializeMatrix();
-    table.printMatrix();
+    table.PrintMatrix();
 
 
+    System.out.println("\n Populating matrix...matrix populated");
+    table.PopulateMatrix();
+    System.out.println("\n Printing matrix:");
+    table.PrintMatrix();
+
+
+    System.out.println("\n Flipping matrix...matrix flipped");
+    table.FlipMatrix();
+    System.out.println("\n Printing flipped matrix:");
+    table.PrintMatrix();
+
+
+    input.close();
   }
 }
